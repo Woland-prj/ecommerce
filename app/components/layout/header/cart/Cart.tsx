ac81a0648388e1f19b403980c97fe682016618f1
Woland-prj <nzimin93@gmail.com>
2023-08-28 12:21:19 +0300

    cart drawer

--- app/components/layout/header/cart/Cart.tsx
+++ app/components/layout/header/cart/Cart.tsx
@@ -1,24 +1,55 @@
-import { FC, useState } from 'react'
+import { FC, useRef, useState } from 'react'
 import styles from './Cart.module.scss'
 import CartItem from './cart-item/CartItem'
 import { cart } from '@/data/cart.data'
+import {
+	Drawer,
+	DrawerBody,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerOverlay,
+	DrawerContent,
+	DrawerCloseButton,
+	Button
+} from '@chakra-ui/react'
 
 const Cart: FC = () => {
 	const [isOpen, setIsOpen] = useState(false)
+	const btnRef = useRef<HTMLButtonElement>(null)
 
 	return (
 		<div className={styles['wrapper-cart']}>
-			<button className={styles.heading} onClick={() => setIsOpen(!isOpen)}>
+			<button
+				className={styles.heading}
+				onClick={() => setIsOpen(!isOpen)}
+				ref={btnRef}
+			>
 				<span className={styles.badge}>1</span>
 				<span className={styles.text}>my basket</span>
 			</button>
-			{isOpen && (
-				<div className={styles.cart}>
-					{cart.map(item => (
-						<CartItem key={item.id} item={item} />
-					))}
-				</div>
-			)}
+			<Drawer
+				isOpen={isOpen}
+				placement='right'
+				onClose={() => setIsOpen(false)}
+				finalFocusRef={btnRef}
+			>
+				<DrawerOverlay />
+				<DrawerContent>
+					<DrawerCloseButton />
+					<DrawerHeader>Cart</DrawerHeader>
+
+					<DrawerBody>
+						<div className={styles.cart}>
+							{cart.map(item => (
+								<CartItem key={item.id} item={item} />
+							))}
+						</div>
+					</DrawerBody>
+					<DrawerFooter>
+						<Button colorScheme='red'>Checkout</Button>
+					</DrawerFooter>
+				</DrawerContent>
+			</Drawer>
 		</div>
 	)
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '@/ui/chakra.config'
 import '@/assets/globals.scss'
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	)
 }
 
 export default App
